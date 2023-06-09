b6afb42c80e7dcfc6966abe83285fb27267dfd93
Cossy12 <cosmasmutuku001@gmail.com>
2023-06-08 19:50:58 -0700

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    # printing command line args
-    while (len(argv) > 1):
-        if (len(argv) == 2):
-            print("{} argument:".format(len(argv) - 1))
-            print("{}: {}".format(len(argv) - 1, argv[1]))
-            exit()
-        else:
-            print("{} arguments:".format(len(argv) - 1))
-            for i in range(1, len(argv)):
-                print("{}: {}".format(i, argv[i]))
-            exit()
-    print("0 arguments.")
+    import sys
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[argc]))
+    else:
+        print("{} arguments:".format(argc))
+        for i in range(argc):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
+
